096591b6cac3b8507dfd25dfc9f15a1e28fdc038
RajeevPal12 <rajivkumar.p100@gmail.com>
2020-10-15 10:45:58 +0900

    added code for factorial programs

--- FactorialJava/src/com/tester/FactorialTest.java
+++ FactorialJava/src/com/tester/FactorialTest.java
@@ -1,10 +1,20 @@
 package com.tester;
 
-public class FactorialTest {
+import java.util.Scanner;
 
+public class FactorialTest {
+public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Start Factorial Project !");
+		System.out.println("Factoril Project start");
+		int num= Integer.valueOf(sc.nextLine());
+		int numFact=getFactorial(num);
+		System.out.println("Factoril of Num"+num +" is :"+numFact);
 
 	}
 
+	public static int getFactorial(int num) {
+		if(num==1) return num;
+		
+		return num*getFactorial(num-1);
+	}
 }
